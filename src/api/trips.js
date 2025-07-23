cd9662f7838618f9d5c295207bd63fdb06cd32f1
Kartik Kanzode <kartikkanzode@gmail.com>
2025-07-23 20:01:40 +0530

    Update trips.js

--- src/api/trips.js
+++ src/api/trips.js
@@ -3,25 +3,25 @@ import axios from "axios";
 import config from "../config";
 
 const api = axios.create({
-  baseURL: config.API_BASE_URL + "/trips",
+  baseURL: config.API_BASE_URL, // Already includes /api
 });
 
 export const getTrips = async () => {
-  return api.get("/");
+  return api.get("/trips");
 };
 
 export const getTrip = async (id) => {
-  return api.get(`/${id}`);
+  return api.get(`/trips/${id}`);
 };
 
 export const createTrip = async (data) => {
-  return api.post("/", data);
+  return api.post("/trips", data);
 };
 
 export const updateTrip = async (id, data) => {
-  return api.put(`/${id}`, data);
+  return api.put(`/trips/${id}`, data);
 };
 
 export const deleteTrip = async (id) => {
-  return api.delete(`/${id}`);
+  return api.delete(`/trips/${id}`);
 };
